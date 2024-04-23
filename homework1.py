64e41e93593ad834a835eeefc6e47cad7c38f5b5
VOLKKODAV <167250805+VOLKKODAV@users.noreply.github.com>
2024-04-24 02:05:54 +0300

    homework1.py

--- homework1.py
+++ homework1.py
@@ -1,6 +1,6 @@
-num1 = 5
-num2 = 3
-sum([num1 + num2])
-print(num1 + num2)
-result = 8 + (num1 * num2)
+num1 = 3
+num2 = 5
+sum: int = num1 + num2
+print(sum)
+result = sum + num1 * num2
 print(result)
